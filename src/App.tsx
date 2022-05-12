ef97cfdf82db058011a9f9613cdb438ed258c117
Hyosub Song <hyosub@redrocket.co.kr>
2022-05-13 00:09:40 +0900

    Counter App 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { observer } from "mobx-react";
+import store from "./store";
 
-function App() {
-  return <div></div>;
-}
+const App: React.FC = observer(() => {
+  const { countClass, countObject } = store;
+
+  return (
+    <div>
+      <div>
+        <h1>Count Class </h1>
+        <div>number: {countClass.number}</div>
+        <button onClick={() => countClass.decrease()}>-</button>
+        <button onClick={() => countClass.increase()}>+</button>
+      </div>
+      <div>
+        <h1>Count Object</h1>
+        <div>number: {countObject.number}</div>
+        <button onClick={() => countObject.decrease()}>-</button>
+        <button onClick={() => countObject.increase()}>+</button>
+      </div>
+    </div>
+  );
+});
 
 export default App;
